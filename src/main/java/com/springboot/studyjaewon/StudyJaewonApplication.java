930a55ece8817a63dc7c06b19323a823b0caa655
JAEWON-A46X\jaewon <jaewun1@gmail.com>
2022-07-12 20:53:08 +0900

    init commit

---
+++ src/main/java/com/springboot/studyjaewon/StudyJaewonApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.studyjaewon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJaewonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyJaewonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/studyjaewon/StudyJaewonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.studyjaewon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyJaewonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
